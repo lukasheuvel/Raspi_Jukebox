543f5adbcb53743c7389bc736fb73c523121e3d8
lukasheuvel <groteheuvel@hotmail.com>
2019-11-03 18:25:20 +0000

    added GPIO.setwarnings(False) to buttontrigger script)

--- buttontrigger.py
+++ buttontrigger.py
@@ -6,6 +6,7 @@ print("kies een plaat\nde plaat die je kiest triggert bij iedere knopdruk")
 selected_nr = input("locatie (e.g. V8):").strip()
 print("locatie nummer {} geselecteerd.\n\nStarting Script\n".format(selected_nr))
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup(4, GPIO.OUT, initial = GPIO.HIGH)
