123c9a8acd600b1220ae6a897c41afb37cc04b75
lukasheuvel <groteheuvel@hotmail.com>
2019-11-03 18:23:10 +0000

    wrote buttontrigger.py script

--- buttontrigger.py
+++ buttontrigger.py
@@ -1,6 +1,17 @@
 import RPi.GPIO as GPIO
+from datetime import datetime
+import interface
 
+print("kies een plaat\nde plaat die je kiest triggert bij iedere knopdruk")
+selected_nr = input("locatie (e.g. V8):").strip()
+print("locatie nummer {} geselecteerd.\n\nStarting Script\n".format(selected_nr))
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(4, GPIO.OUT, initial = GPIO.HIGH)
 
 while True:
-    
+    if GPIO.input(24) == GPIO.HIGH:
+        print("Sending signal for {} | {}".format(selected_nr, datetime.now))
+        signal = interface.encode(selected_nr)
+        interface.send_gpio_signal(signal)
